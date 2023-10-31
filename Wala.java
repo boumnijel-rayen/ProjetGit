ac5e32f314bc29ce6fe9259675bf7b37578a3d2d
wala-amiri <walaamiri02@gmail.com>
2023-10-31 21:29:37 +0100

    Second Commit

---
+++ Wala.java
@@ -0,0 +1,42 @@
+public class Wala {
+
+    private int id;
+    private String nom;
+    private String prenom;
+  // Constructeurs
+
+    public Wala() {
+    }
+
+    public Wala(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+  // Méthodes d'accès en lecture et écriture
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+
+}
