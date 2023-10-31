e94cf097948f81276baebd0edd80d2c85a42ab2e
Rayen Boumnijel <boumnijelrayen9@gmail.com>
2023-10-31 20:16:24 +0100

    Initialisation

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a new instance of the class
+        // and call the method
+        new Main().printHelloWorld();
+    }
+
+    // Method to print "Hello World!"
+    public void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
