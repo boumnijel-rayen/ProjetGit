0b3f806b5dd4c0f933d735d0668697dce0d3ce8a
ons2 <bouazizions@gmail.com>
2023-10-31 22:02:50 +0100

    new-commit-3

--- Ons.java
+++ Ons.java
@@ -1,7 +1,7 @@
 public class Hello {
     private String name;
     private int age;
-    private String adresse; 
+    private String adresse;
 
     public Hello {
 
@@ -12,5 +12,6 @@ public class Hello {
         System.out.println("Age: " + age);
         System.out.println("Adress: " + adresse);
         System.out.println("My name is " + name + "and my age is" + age);
+        System.out.println("Hello");
     }
 }
