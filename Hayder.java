497c4a623a1fc20a0ad042570a234ca25fc65c47
hayder mlik <Hayder-10>
2023-10-31 21:50:19 +0100

    Ajout des getters et setters

--- Hayder.java
+++ Hayder.java
@@ -4,4 +4,84 @@ public class Hayder {
    private String prenom;
    private int tel;
    private int id;
+
+   public String getCin() {
+    return cin;
+}
+
+public void setCin(String cin) {
+    this.cin = cin;
+}
+
+public String getNom() {
+    return nom;
+}
+
+public void setNom(String nom) {
+    this.nom = nom;
+}
+Prenom:
+java
+Copy code
+private String prenom;
+
+public String getPrenom() {
+    return prenom;
+}
+
+public void setPrenom(String prenom) {
+    this.prenom = prenom;
+}
+Tel:
+java
+Copy code
+private int tel;
+
+public int getTel() {
+    return tel;
+}
+
+public void setTel(int tel) {
+    this.tel = tel;
+}
+
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public String getPrenom() {
+    return prenom;
+}
+
+public void setPrenom(String prenom) {
+    this.prenom = prenom;
+}
+
+
+
+public int getTel() {
+    return tel;
+}
+
+public void setTel(int tel) {
+    this.tel = tel;
+}
+
+
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+
+
 }
