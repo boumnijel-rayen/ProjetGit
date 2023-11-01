6df99155a7d4ab2d4cd26e09e5d91b7a353299b7
Rayen Boumnijel <boumnijelrayen9@gmail.com>
2023-11-01 21:08:16 +0100

    ajoute constructeur paramétré

--- rayen.java
+++ rayen.java
@@ -5,6 +5,13 @@ public class rayen{
     public String adress;
     public String phone;
 
+    public rayen(String name, int age, String adress, String phone){
+        this.name = name;
+        this.age = age;
+        this.adress = adress;
+        this.phone = phone;
+    }
+
     
     public rayen(){
         System.out.println("Hello World!");
